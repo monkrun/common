0de4bb838b903aab55d6b24c670897ef5181dc93
隋龙飞@视频结构化 <longfeisui@deepglint.com>
2019-06-18 15:01:30 +0800

    添加错误声明

--- models/fighter.go
+++ models/fighter.go
@@ -1,11 +1,18 @@
 package models
 
+import "errors"
+
 type AttackType int
 
 const (
 	_                  AttackType = iota
 	AttackTypePhysical            //物理攻击方式
 	AttackTypeMagic               //魔法攻击方式
+)
+
+var (
+	ErrorCurrentFighterIsDead = errors.New("current fighter is dead")
+	ErrorTargetFighterIsDead  = errors.New("target fighter is dead")
 )
 
 //斗士基本属性
