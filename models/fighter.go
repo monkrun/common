e1ece94b8181851e0dc9cd847330d0d92e77eb88
logan <logan.sui@loganx.org>
2019-06-19 00:50:30 +0800

    设定攻击方式

--- models/fighter.go
+++ models/fighter.go
@@ -20,6 +20,7 @@ type Fighter struct {
 	Life           int32      //生命值
 	Mana           int32      //魔法值
 	AttackInterval int64      //物理攻击时间间隔，单位毫秒
-	Attack         int32      //攻击基准值
+	AttackMin      int32      //攻击最小值
+	AttackMax      int32      //攻击最大值
 	AttackType     AttackType //攻击方式
 }
